8946c39c0359a674a46cbd87ee79c32e87ecb4ec
abdelkarim hajji <hajji.karim.solicode@gmail.com>
2024-08-26 12:08:27 +0100

    add this update

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,68 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 13:07:16 by ahajji            #+#    #+#             */
-/*   Updated: 2024/08/25 15:53:28 by ahajji           ###   ########.fr       */
+/*   Updated: 2024/08/26 12:06:29 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void    errorParse(int state)
+{
+    if(state == 0)
+        std::cout << "error in arg" << std::endl;
+    else if(state == 1)
+        std::cout << "error in parse" << std::endl;
+    std::exit(1);
+}
+
+void    checkDateBitc(std::string const& line)
+{
+    size_t pos = line.find(',');
+    if(pos != std::string::npos)
+    {
+        for(int i = pos + 1; i < line.length(); i++)
+        {
+            if((i == (pos + 1)) && std::isdigit(line[i]) == false)
+                errorParse(1);
+            else if (i != (pos + 1) && std::isdigit(line[i]) == false && line[i] != '.')
+                errorParse(1);
+            else if(line[i + 1] == '\0' )     
+        }
+        std::cout << line[i] << std::endl;
+        // if(line[--i] )
+    }
+}
+
 int main()
 {
-    
+    std::ifstream file("data.csv");
+    std::string line;
+    int count = 0;
+    if(file.is_open())
+    {
+        while (std::getline(file, line))
+        {
+            for(int i = 0; i < line.length(); i++)
+            {
+                if(count == 0 && line != "date,exchange_rate")
+                {
+                    std::cout << line << " this is i " << i<< "\n";                    
+                    errorParse(1);
+                }
+                else if(count != 0)
+                    checkDateBitc(line);
+            }
+            count++;
+        }
+        
+    }
+    else
+    {
+        errorParse(0);
+    }
     return 0;
 }
