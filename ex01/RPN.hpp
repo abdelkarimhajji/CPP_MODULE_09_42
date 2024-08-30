fb19b4fb1f59d36395add01ffef8360a52192408
abdelkarimhajji <hajji.karim.solicode@gmail.com>
2024-08-30 12:08:38 +0100

    make update to the first exercise

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/24 13:07:16 by ahajji            #+#    #+#             */
-/*   Updated: 2024/08/30 12:08:08 by ahajji           ###   ########.fr       */
+/*   Created: 2024/08/30 11:38:45 by ahajji            #+#    #+#             */
+/*   Updated: 2024/08/30 11:42:58 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "BitcoinExchange.hpp"
+#ifndef RPN_HPP
+#define RPN_HPP
 
-int main(int ac, char **av)
+#include <iostream>
+
+class RPN
 {
-    if(ac != 2)
-    {
-        BitcoinExchange bit(av[1]);
-        bit.displayBitcoins();
-    }
-    else
-    {
-        std::cout << "error in arg\n" << std::endl;
-        exit(1);
-    }
-    return 0;
-}
+    private:
+        
+    public:
+        
+};
+
+#endif
