72a48ccbb9875096e5b87ef1593c305f103d9b17
abdelkarimhajji <hajji.karim.solicode@gmail.com>
2024-09-11 08:16:49 +0100

    i add another data structer

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 14:20:55 by ahajji            #+#    #+#             */
-/*   Updated: 2024/09/10 18:47:35 by ahajji           ###   ########.fr       */
+/*   Updated: 2024/09/11 08:16:12 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ void    PmergeMe::checkErrors(char **av)
                 error("Is not digit");
         }
         args.push_back(std::atoi(av[i]));
+        args_.push_back(std::atoi(av[i]));
     }
 }
 
@@ -131,6 +132,59 @@ void    PmergeMe::recursionFunction(int sizePair, int numberPairs)
     }
 }
 
+void    PmergeMe::recursionFunction_(int sizePair, int numberPairs)
+{
+    std::deque<int> rest;
+    std::deque<std::deque<std::deque<int> > > arrList;
+    
+    if(numberPairs < 1)
+        return;
+    int count = numberPairs * sizePair;
+    while (count < args.size())
+    {
+        rest.push_back(args[count]);
+        count++;
+    }
+    
+    for (int i = 0; i < numberPairs * sizePair; )
+    {
+        std::deque<std::deque<int> > pair;
+        for (int k = 0; k < 2; k++)
+        {
+            std::deque<int> half;
+            for (int j = 0; j < sizePair/2; j++)
+                half.push_back(args[i++]);
+            pair.push_back(half);
+        }
+        arrList.push_back(pair);
+    }
+
+    for (int i = 0; i < numberPairs; i++)
+    {
+        if(arrList[i][0].back() > arrList[i][1].back())
+            std::swap(arrList[i][0], arrList[i][1]);
+    }
+    args_.clear();
+    for (int i = 0; i < numberPairs;i++ )
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k = 0; k <sizePair/2; k++)
+            {
+                
+                args_.push_back(arrList[i][j][k]);
+            }
+            
+        }
+    }
+    recursionFunction(sizePair * 2, numberPairs / 2);
+    if(!rest.empty())
+    {
+        for(int i = 0; i < rest.size();i++)
+            args_.push_back(rest[i]);
+    }
+}
+
 std::vector<int>    PmergeMe::sortAll()
 {
     std::vector<int> largeNumbers;
@@ -161,10 +215,85 @@ std::vector<int>    PmergeMe::sortAll()
     return (largeNumbers);
 }
 
+std::deque<int>    PmergeMe::sortAll_()
+{
+    std::deque<int> largeNumbers;
+    std::deque<int> smallNumbers;
+    for(int i = 0; i < args.size(); i++)
+    {
+        if(i % 2 != 0)
+            smallNumbers.push_back(args[i]);
+        else    
+            largeNumbers.push_back(args[i]);
+    }
+    int tmp;
+    int valid = 0;
+    int j = 0;
+    for (int j = 0; j < largeNumbers.size(); j++) {
+        for (int i = j + 1; i < largeNumbers.size(); i++) {
+            if (largeNumbers[j] > largeNumbers[i]) {
+                std::swap(largeNumbers[j],largeNumbers[i]);
+            }
+        }
+    }
+    for(int i = 0; i < smallNumbers.size(); i++)
+    {
+        std::deque<int>::iterator lower = std::lower_bound(largeNumbers.begin(), largeNumbers.end(), smallNumbers[i]);
+        largeNumbers.insert(lower, smallNumbers[i]);
+    }
+    return (largeNumbers);
+}
+
 void    PmergeMe::sort()
 {
+    if(ac < 2)
+        error("You should be Pass to constructer numbers and number of numbers");
+    std::vector<int> before = this->args;
+    std::clock_t start = std::clock();
     recursionFunction(2, args.size()/2);
     args = sortAll();
-    for(int i = 0; i < args.size(); i++)
-        std::cout << args[i] << std::endl;
+    std::clock_t end = std::clock();
+    double diff = 1000.0 * (end-start) / CLOCKS_PER_SEC;
+    std::cout << "Before: ";
+    for(int i = 0; i < 5; i++)
+        std::cout  << before[i] << " ";
+    if(args.size() > 5)
+        std::cout << " [...]";
+    std::cout << "\n";
+    std::cout << "After: ";
+    for(int i = 0; i < 5; i++)
+        std::cout << args[i] << " ";
+    if(args.size() > 5)
+        std::cout << " [...]";
+    std::cout << "\n";
+    std::cout << "Time to process a range of " << args.size() << " elements with std::[..] : " << diff << " us\n";
+    std::clock_t start2 = std::clock();
+    std::deque<int> before_ = this->args_;
+    recursionFunction_(2, args_.size()/2);
+    args_ = sortAll_();
+    std::clock_t end2 = std::clock();
+    double diff2 = 1000.0 * (end-start) / CLOCKS_PER_SEC;
+    std::cout << "Time to process a range of " << args_.size() << " elements with std::[..] : " << diff2 << " us\n";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 14:17:55 by ahajji            #+#    #+#             */
-/*   Updated: 2024/09/10 15:42:27 by ahajji           ###   ########.fr       */
+/*   Updated: 2024/09/11 07:55:16 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,21 @@
 #include<string>
 #include <sstream>
 #include <climits>
+#include <ctime>
+#include <deque>
 
 class   PmergeMe
 {
     private:
         std::vector<int> args;
+        std::deque<int> args_;
         int ac;
         void    checkErrors(char **av);
         void    cheackDuplicate();
         void    recursionFunction(int sizePair, int numberPairs);
+        void    recursionFunction_(int sizePair, int numberPairs);
         std::vector<int>    sortAll();
+        std::deque<int>    sortAll_();
     public:
         PmergeMe();
         PmergeMe(int ac, char **av);
@@ -36,6 +41,7 @@ class   PmergeMe
         PmergeMe& operator=(const PmergeMe& other);
         static void error(std::string arg);
         void   sort();
+        void   sort_();
         ~PmergeMe();
 };
 
